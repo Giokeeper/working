03a0e874c7f1f3341336c85f0b2ddc2bd9196a8f
Giokeeper <giokeeper@gmail.com>
2017-05-09 12:55:42 +0300

    в процессе

--- Task - 2 - CSS_Transforms_Trasnitions_Animations/scripts/main.js
+++
@@ -1,7 +0,0 @@
-var openMenuButton = document.getElementsByClassName('open-menu')[0],
-    menuWrapper = document.getElementsByClassName('menu-wrapper')[0];
-
-
-openMenuButton.addEventListener('click', function() {
-    menuWrapper.classList.toggle('active')
-});
